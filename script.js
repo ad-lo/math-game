eb7eb03c374d19c2cf6168205cc45107a7a555b7
Chris Sayer <crichardsayer@gmail.com>
2022-11-22 14:42:39 -0500

    version 5

--- script.js
+++ script.js
@@ -83,6 +83,27 @@ function createEquations() {
 //   itemContainer.appendChild(bottomSpacer);
 // }
 
+// Display 3, 2, 1, GO!
+function countdownStart() {
+countdown.textContent = '3';
+setTimeout(() => {
+  countdown.textContent = '2';
+}, 1000);
+setTimeout(() => {
+  countdown.textContent = '1';
+}, 2000);
+setTimeout(() => {
+  countdown.textContent = 'GO!';
+}, 3000);
+}
+
+// Navigate from splash page to countdown page
+function showCountdown() {
+  countdownPage.hidden = false;
+  splashPage.hidden = true;
+  countdownStart();
+}
+
 // get value from selected radio button
 function getRadioValue() {
   let radioValue;
@@ -100,6 +121,9 @@ function selectedQuestionAmount(e) {
   e.preventDefault();
   questionAmount = getRadioValue();
   console.log('question amount', questionAmount);
+ if (questionAmount) {
+  showCountdown();
+ }
 }
 
 startForm.addEventListener('click', () => {
