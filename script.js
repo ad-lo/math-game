5fb06051d2744e6124f987786a91ed49829a9309
Chris Sayer <crichardsayer@gmail.com>
2022-11-29 14:39:06 -0500

    version 9

--- script.js
+++ script.js
@@ -40,6 +40,41 @@ let finalTimeDisplay = '0.0s';
 // Scroll
 let valueY = 0;
 
+// Reset Game
+function playAgain() {
+  gamePage.addEventListener('click', startTimer);
+  scorePage.hidden = true;
+  splashPage.hidden = false;
+  equationsArray = [];
+  playerGuessArray = [];
+  valueY = 0;
+  playAgainBtn.hidden = true;
+}
+
+// Show Score Page
+function showScorePage() {
+  // show play again button after 1s
+  setTimeout(() => {
+playAgainBtn.hidden = false;
+  }, 1000);
+  gamePage.hidden = true;
+  scorePage.hidden = false;
+
+}
+
+// format & display time to DOM
+function scoresToDOM() {
+  finalTimeDisplay = finalTime.toFixed(1);
+  baseTime = timePlayed.toFixed(1);
+  penaltyTime = penaltyTime.toFixed(1);
+  baseTimeEl.textContent = `Base Time ${baseTime}s`;
+  penaltyTimeEl.textContent = `Penalty: +${penaltyTime}s`;
+  finalTimeEl.textContent = `${finalTimeDisplay}s`;
+  // Scroll to Top, go to score page
+  itemContainer.scrollTo({ top:0, behavior: 'instant'});
+  showScorePage();
+}
+
 // Stop Timer, Process Results, go to Score Page
 function checkTime() {
   console.log(timePlayed);
@@ -57,6 +92,7 @@ function checkTime() {
     });
     finalTime = timePlayed + penaltyTime;
     console.log('time', timePlayed, 'penalty', penaltyTime, 'final', finalTime)
+    scoresToDOM();
   }
 }
 
